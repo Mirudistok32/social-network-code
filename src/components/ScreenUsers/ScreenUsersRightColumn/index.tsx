a197a051220ff138cd46f3b081aa6e71e0a91b86
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-05 22:03:20 +0300

    Refactor ScreenUsers

--- src/components/ScreenUsers/ScreenUsersRightColumn/index.tsx
+++ src/components/ScreenUsers/ScreenUsersRightColumn/index.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import './ScreenUsersRightColumn.scss';
 import { UserMessages } from '../../ScreenMessages/ScreenMessagesRightColumn/UserMessages';
 
+type PropsType = {
+  title: string
+}
 
-export const ScreenUsersRightColumn = () => {
+export const ScreenUsersRightColumn: React.FC<PropsType> = ({ title }) => {
   return (
     <div className="screen-friends-right-column">
       <div className="screen-friends-right-column__header">
         <ul className="screen-friends-right-column__list">
           <li className="screen-friends-right-column__item">
-            <button className="screen-friends-right-column__btn">Все друзья</button>
+            <button className="screen-friends-right-column__btn">{title}</button>
           </li>
         </ul>
       </div>
       <div className="screen-friends-right-column__users">
         <div className="screen-friends-right-column__user">
-          <UserMessages />
+          <UserMessages link="/profile/" />
         </div>
       </div>
     </div>
