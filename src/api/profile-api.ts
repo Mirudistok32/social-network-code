6e629d80a78b4432322002856fe0457e98d9b8de
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-07 16:34:43 +0300

    Install API request status profile

--- src/api/profile-api.ts
+++ src/api/profile-api.ts
@@ -3,5 +3,8 @@ import { instance, GetProfileType } from "./api";
 export const profileAPI = {
     getProfile: (id: number) => {
         return instance.get<GetProfileType>(`/profile/${id}`).then(res => res.data)
+    },
+    getStatusProfile: (id: number) => {
+        return instance.get<string>(`profile/status/${id}`).then(res => res.data)
     }
 };
