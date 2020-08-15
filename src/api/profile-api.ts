2088781d87ffed1fe226eaafadced144c4557883
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-15 23:49:22 +0300

    Add api put

--- src/api/profile-api.ts
+++ src/api/profile-api.ts
@@ -1,10 +1,23 @@
 import { instance, GetProfileType } from "./api";
 
+
+type DataType = {
+
+}
+type StatusProfileType = {
+    resultCode: number
+    messages: Array<string>
+    data: DataType
+}
+
 export const profileAPI = {
     getProfile: (id: number) => {
         return instance.get<GetProfileType>(`profile/${id}`).then(res => res.data)
     },
     getStatusProfile: (id: number) => {
         return instance.get<string>(`profile/status/${id}`).then(res => res.data)
+    },
+    setStatusProfile: (status: string, id: number) => {
+        return instance.put<StatusProfileType>(`profile/status/${id}`, { status }).then(res => res.data)
     }
 };
