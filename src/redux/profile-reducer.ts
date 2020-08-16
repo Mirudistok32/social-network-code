46959cb338e1b76920f04dbc62eed8e9cb8a61f3
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-16 18:43:23 +0300

    Create thunk for set my profile photos

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -2,15 +2,18 @@ import { GetProfileType } from "../api/api"
 import { InferActionsTypes, AppStateType } from "./store"
 import { profileAPI } from "../api/profile-api"
 import { ThunkAction } from "redux-thunk"
+import { PhotosType } from "./users-reducer"
 
 
 const initialState = {
     profile: null as GetProfileType | null,
-    status: ''
+    status: '',
+    photoMyProfile: null as PhotosType | null
 }
 
 type InitialStateType = typeof initialState
 type ActionsTypes = InferActionsTypes<typeof actions>
+type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes>
 
 export const profileReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
     switch (action.type) {
@@ -20,6 +23,9 @@ export const profileReducer = (state = initialState, action: ActionsTypes): Init
         case 'SN/PROFILE/SET_STATUS_PROFILE': {
             return { ...state, status: action.status }
         }
+        case 'SN/PROFILE/SET_MY_PHOTO_PROFILE': {
+            return { ...state, photoMyProfile: action.photos }
+        }
     }
     return state
 }
@@ -27,11 +33,12 @@ export const profileReducer = (state = initialState, action: ActionsTypes): Init
 
 const actions = {
     setProfile: (profile: GetProfileType) => ({ type: 'SN/PROFILE/SET_PROFILE', profile } as const),
-    setStatusProfile: (status: string) => ({ type: 'SN/PROFILE/SET_STATUS_PROFILE', status } as const)
+    setStatusProfile: (status: string) => ({ type: 'SN/PROFILE/SET_STATUS_PROFILE', status } as const),
+    setMyPhotoProfile: (photos: PhotosType) => ({ type: 'SN/PROFILE/SET_MY_PHOTO_PROFILE', photos } as const)
 }
 
 
-export const setProfileThunk = (id: number): ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes> => {
+export const setProfileThunk = (id: number): ThunkType => {
 
     return async (dispatch) => {
 
@@ -40,16 +47,23 @@ export const setProfileThunk = (id: number): ThunkAction<Promise<void>, AppState
 
         dispatch(actions.setProfile(profile))
         dispatch(actions.setStatusProfile(status))
+
     }
 }
 
+export const setMyProfilePhotos = (id: number): ThunkType => {
+    return async (dispatch) => {
+        const profile = await profileAPI.getProfile(id)
+        dispatch(actions.setMyPhotoProfile(profile.photos))
+    }
+}
 
 //Санка для изменения статуса, здесь мы делаем put запрос на сервер
-export const setProfileStatusThunk = (status: string): ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes> => {
+export const setProfileStatusThunk = (status: string): ThunkType => {
 
     return async (dispatch) => {
         const statusResult = await profileAPI.setStatusProfile(status)
-        
+
         //Если код статуса 0, то все успешно, и мы перезаписываем статус
         if (statusResult.resultCode === 0) {
             dispatch(actions.setStatusProfile(status))
