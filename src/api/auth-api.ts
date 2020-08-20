59a257f2e5eacd20d5b1c15caade408b725f0c32
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-20 19:36:00 +0300

    Create api for request post loginIn

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -23,8 +23,8 @@ export const authAPI = {
     getAuthMe: () => {
         return instance.get<GetAuthMeType<GetAuthMeDataType>>(`auth/me`).then(res => res.data)
     },
-    loginIn: () => {
-        return instance.post<GetAuthMeType<LoginIn>>(`auth/login`).then(res => res.data)
+    loginIn: (email: string, password: string, rememberMe: boolean = false) => {
+        return instance.post<GetAuthMeType<LoginIn>>(`auth/login`, {email, password, rememberMe}).then(res => res.data)
     },
     loginOut: () => {
         return instance.delete<GetAuthMeType<LoginOut>>(`auth/login`).then(res => res.data)
