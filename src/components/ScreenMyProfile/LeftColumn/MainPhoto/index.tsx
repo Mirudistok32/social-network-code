a1f091d8cf1f2e40adddc240306288c90730734e
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-25 12:08:42 +0300

    Refactor MainPhoto component

--- src/components/ScreenMyProfile/LeftColumn/MainPhoto/index.tsx
+++ src/components/ScreenMyProfile/LeftColumn/MainPhoto/index.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MainPhoto.scss';
 import avatarDefault from '../../../../assets/images/octopus.svg'
 import { PhotosType } from '../../../../redux/users-reducer';
 import { useSelector } from 'react-redux';
-import { getAuthIsAutorizationSelect } from '../../../../selects/auth-select';
+import { useParams } from 'react-router-dom';
+import { AppStateType } from '../../../../redux/store';
 // import avatarDefault from '../../assets/images/default-icon.jpg'
 
 export type PropsType = {
   photos: PhotosType
 }
 
-export const MainPhoto: React.FC<PropsType> = (props) => {
+export const MainPhoto: React.FC<PropsType> = React.memo((props) => {
 
   const { photos } = props
 
-  const isAuth = useSelector(getAuthIsAutorizationSelect)
+  const [isShow, setIsShow] = useState(false)
+  const myId = useSelector((state: AppStateType) => state.authReducer.id)
+  const { id } = useParams()
+
+  useEffect(() => {
+    if (myId) {
+      if (id) {
+        if (myId == id) setIsShow(true)
+      }
+    }
+  }, [myId, id])
 
   let photoURL = photos.small ? photos.small : photos.large ? photos.large : avatarDefault
 
@@ -22,7 +33,7 @@ export const MainPhoto: React.FC<PropsType> = (props) => {
     <div className="main-photo main-photo__box" >
       <img className="main-photo" src={photoURL} alt="avatar" />
       {
-        isAuth && (
+        isShow && (
           <label className="main-photo__change-box">Выбрать фото
             <input className="main-photo__change" type="file" />
           </label>
@@ -30,4 +41,4 @@ export const MainPhoto: React.FC<PropsType> = (props) => {
       }
     </div>
   );
-}
+})
