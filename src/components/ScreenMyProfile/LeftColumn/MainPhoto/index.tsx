033eff7d65286455a58bf389bbe4a7ed21c9a281
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-25 11:43:32 +0300

    Create change photo on main profile

--- src/components/ScreenMyProfile/LeftColumn/MainPhoto/index.tsx
+++ src/components/ScreenMyProfile/LeftColumn/MainPhoto/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './MainPhoto.scss';
 import avatarDefault from '../../../../assets/images/octopus.svg'
 import { PhotosType } from '../../../../redux/users-reducer';
+import { useSelector } from 'react-redux';
+import { getAuthIsAutorizationSelect } from '../../../../selects/auth-select';
 // import avatarDefault from '../../assets/images/default-icon.jpg'
 
 export type PropsType = {
@@ -12,9 +14,16 @@ export const MainPhoto: React.FC<PropsType> = (props) => {
 
   const { photos } = props
 
+  const isAuth = useSelector(getAuthIsAutorizationSelect)
+
   let photoURL = photos.small ? photos.small : photos.large ? photos.large : avatarDefault
 
   return (
-    <img className="main-photo" src={photoURL} alt="avatar" />
+    <div className="main-photo main-photo__box" >
+      <img className="main-photo" src={photoURL} alt="avatar" />
+      {
+        isAuth && <label className="main-photo__change-box">Выбрать фото<input className="main-photo__change" type="file" /></label>
+      }
+    </div>
   );
 }
